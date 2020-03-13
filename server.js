889dba952c9e4c1d990d0b4fb96cc10bb5b42fb5
Luis PM <luisp237@gmail.com>
2020-03-13 12:54:33 -0400

    server full CRUD, NPX React app

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 const server = express();
+const cors = require('cors');
+
 
 server.use(express.json());
 server.use(helmet());
+server.use(cors());
 
 const actionRouter = require("./data/actionRouter.js");
 const projectRouter = require("./data/projectRouter.js");
